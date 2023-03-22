06319630964b871062ea3d84d5945f86716ef288
Paolo Debuque <pdebuque@gmail.com>
2023-03-22 10:46:30 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// internal components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Canvas from './components/Canvas'
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Canvas />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
